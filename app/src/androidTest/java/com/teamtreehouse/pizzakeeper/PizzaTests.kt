8057e7d31086bde3d83b3d54ee93ac5bca03e154
bobtal <talevski.boban@gmail.com>
2018-09-14 14:35:58 +0200

    End of Saving a Pizza's Toppings video

--- app/src/androidTest/java/com/teamtreehouse/pizzakeeper/PizzaTests.kt
+++ app/src/androidTest/java/com/teamtreehouse/pizzakeeper/PizzaTests.kt
@@ -4,8 +4,7 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import com.teamtreehouse.pizzakeeper.data.Pizza
-import com.teamtreehouse.pizzakeeper.data.PizzaDatabase
+import com.teamtreehouse.pizzakeeper.data.*
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,14 +23,33 @@ class PizzaTests {
     val testPizza = Pizza(0, "Hawaiian", Date())
     val testToppingIds = listOf(1, 7)
 
+    val appContext = InstrumentationRegistry.getTargetContext()
+    val db = Room.databaseBuilder(appContext, PizzaDatabase::class.java, "PizzaDatabase")
+            .build()
+
     @Test
     fun pizzaTest() {
-        val appContext = InstrumentationRegistry.getTargetContext()
-        val db = Room.databaseBuilder(appContext, PizzaDatabase::class.java, "PizzaDatabase")
-                .build()
         db.clearAllTables()
         db.pizzaDao().insert(testPizza)
         val returnedPizza = db.pizzaDao().getPizzaById(testPizza.id)
         assertEquals(testPizza, returnedPizza)
+    }
+
+    @Test
+    fun pizzaToppingTest() {
+        // arrange
+        db.clearAllTables()
+        db.pizzaDao().insert(testPizza)
+        toppings.forEach(db.toppingDao()::insert)
+
+        // act
+        testToppingIds.forEach {
+            val pizzaTopping = PizzaTopping(testPizza.id, it)
+            db.pizzaToppingDao().insert(pizzaTopping)
+        }
+        val returnedToppingIds = db.pizzaToppingDao().getToppingIdsForPizzaId(testPizza.id)
+
+        // assert
+        assertEquals(testToppingIds, returnedToppingIds)
     }
 }
